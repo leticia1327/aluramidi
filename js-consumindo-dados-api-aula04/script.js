869c194c5a48470e5cd86b30de06d6576368eba2
leticia1327 <106753467+leticia1327@users.noreply.github.com>
2023-10-31 09:58:17 -0300

    Add files via upload

--- js-consumindo-dados-api-aula04/script.js
+++ js-consumindo-dados-api-aula04/script.js
@@ -1,22 +1,31 @@
-async function buscaEndereco(cep) {
-    try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
-        }
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
-    } catch (erro) {
-        console.log(erro);
-    }
+
+
+
+
+function tocaSomPom (idElementos) {
+    document.querySelector(idElementosAdulto).play();
+ 
 }
 
-/* --- Lidando com várias requisições ao mesmo tempo com Promise.all ---
 
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+//o que é uma constante...
+const listaDeTeclas = document.querySelectorAll('.tecla'); 
 
-*/
+
+let  contador = 0;
+
+while (contador < listaDeTeclas.length) {
+      const efeito = 
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1; 
+    console.log(contador);
+}
+
+document.querySelector('.tecla)_pom');
+
+
+
+
+
+
