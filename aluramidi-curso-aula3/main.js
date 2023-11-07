5450a3816acb15d333e8e016c97fa8d23e2b4089
leticia1327 <106753467+leticia1327@users.noreply.github.com>
2023-11-07 09:50:27 -0300

    Delete aluramidi-curso-aula3 directory

--- aluramidi-curso-aula3/main.js
+++
@@ -1,8 +0,0 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-listaDeTeclas[0].onclick = tocaSomPom;
