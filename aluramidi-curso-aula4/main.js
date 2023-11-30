c827923c5186b5a4290d04c1247ddc15491669b9
leticia1327 <106753467+leticia1327@users.noreply.github.com>
2023-11-30 10:37:31 -0300

    Update main.js

--- aluramidi-curso-aula4/main.js
+++ aluramidi-curso-aula4/main.js
@@ -1,26 +1,26 @@
 function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-
 }
-document.querySelector('.tecla_pom') .onclick = tocaSomPom;
-
+ 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//Essa função consegue selecionar e ler o arquivo, mas não conseguimos 
-//manipular o elemnto 
-let contador= 0;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-while (contador < listaDeTeclas.length) {
-    const efeito =listaDeTeclas[contador].class[1];
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function()
-    {
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    contador = contador +1;
-    console.log(contador); 
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
-
-
 
